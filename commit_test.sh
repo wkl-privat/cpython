7b375b8bfe2170e51d8700b5f83dcc6878eaa2f3
Witek <wkl@keepit.com>
2026-02-20 14:19:16 +0100

    test commit 1

--- commit_test.sh
+++ commit_test.sh
@@ -8,5 +8,5 @@ for ((i=1; i<=N; i++)); do
   git commit -m "test commit $i"
   git push
   
-  dslagent --account ectug4-kmdj3c-0j4bty --device x3tmdo-t5yfyk-84lcc7 --mode backup --trace-to-console --config /home/wkl/projects/keepit/targets/debian12-amd64/fulltest/cloudbackup.xml --dsl-source-file /home/wkl/workspace/keepit-workspace/src/cloudbackup/agents/dsl/cdl/github/github.cdl
+  /home/wkl/projects/keepit/targets/debian12-amd64/cloudbackup/agents/dsl/dslagent --account ectug4-kmdj3c-0j4bty --device x3tmdo-t5yfyk-84lcc7 --mode backup --trace-to-console --config /home/wkl/projects/keepit/targets/debian12-amd64/fulltest/cloudbackup.xml --dsl-source-file /home/wkl/workspace/keepit-workspace/src/cloudbackup/agents/dsl/cdl/github/github.cdl
 done
